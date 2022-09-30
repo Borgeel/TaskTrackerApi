62c8f5e9167a0b0a6412a0dd1c4451046e90fb18
Irfan <irfancesir93@gmail.com>
2022-10-01 01:09:41 +0200

    added task model and dbcontext

---
+++ TaskTrackerApi/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskTrackerApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<Task> Tasks { get; set; }  
+    }
+}
---
+++ TaskTrackerApi/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTrackerApi.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public DateOnly Date { get; set; } = new DateOnly();
+        public bool Reminder { get; set; }
+    }
+}
